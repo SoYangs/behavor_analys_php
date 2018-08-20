1c355cd560a5e571e89b4fe496b92066ea3f21b1
yangxianci <yangxianci@126.com>
2018-08-20 11:06:33 +0800

    create behabor_analys_php

---
+++ index.php
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    var _maq = _maq || [];
+    _maq.push(['_setAccount', '<?php echo $ip;?>']);
+    (function () {
+        var ma = document.createElement('script');
+        ma.type = 'text/javascript';
+        ma.async = true;
+        ma.src = "./assets/js/behavor.analys.js";
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ma, s);
+    })();
+</script>
+
+<div>刷新就行了</div>
