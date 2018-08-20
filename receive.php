0c409bc4d2643c375e3f225e560d83a552a0ec68
yangxianci <yangxianci@126.com>
2018-08-20 12:35:48 +0800

    create behabor_analys_php

--- receive.php
+++ receive.php
@@ -1,1 +1,18 @@
 <?php
+if($_GET){
+    $data=$_GET['args'];
+    $years = date('Y-m');
+    //设置路径目录信息
+    $url ='./runtime/log/maidian/'.$years.'/'.date('Ymd').'_request_log.txt';
+    //var_dump($url);exit;
+    $dir_name=dirname($url);
+    //目录不存在就创建
+    if(!file_exists($dir_name))
+    {
+        //iconv防止中文名乱码
+        $res = mkdir(iconv("UTF-8", "GBK", $dir_name),0777,true);
+    }
+    $fp = fopen($url,"a");//打开文件资源通道 不存在则自动创建
+    fwrite($fp,var_export($data,true)."\r\n");//写入文件
+    fclose($fp);//关闭资源通道
+}
